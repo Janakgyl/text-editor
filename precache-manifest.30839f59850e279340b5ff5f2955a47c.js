59620b7ce262fbee58186c5854b9a071f2d52d77
Janakgyl <109382044+Janakgyl@users.noreply.github.com>
2022-11-19 22:23:45 +0530

    Updates

---
+++ precache-manifest.30839f59850e279340b5ff5f2955a47c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "10a44f102df84c5d7c74",
+    "url": "/text-editor/static/css/main.deb6b499.chunk.css"
+  },
+  {
+    "revision": "10a44f102df84c5d7c74",
+    "url": "/text-editor/static/js/main.10a44f10.chunk.js"
+  },
+  {
+    "revision": "95ea83b6c7492a61ef7b",
+    "url": "/text-editor/static/js/1.95ea83b6.chunk.js"
+  },
+  {
+    "revision": "31d77044af729c125605",
+    "url": "/text-editor/static/js/2.31d77044.chunk.js"
+  },
+  {
+    "revision": "d9b050ed6fcfde15b20a",
+    "url": "/text-editor/static/js/runtime~main.d9b050ed.js"
+  },
+  {
+    "revision": "f5ba2a7f61d63b1d9c730962f97a3a7c",
+    "url": "/text-editor/index.html"
+  }
+];
